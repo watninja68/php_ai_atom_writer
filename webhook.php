69f7c73e01fc686037999fccda5e34702004864e
karneeshkar <karneeshkar68@gmail.com>
2025-03-26 22:02:09 +0530

    webohook with status

--- webhook.php
+++ webhook.php
@@ -4,24 +4,23 @@ declare(strict_types=1);
 
 require 'vendor/autoload.php';
 
-/*
- * Verify a webhook request using PSR-7 when using a framework that does not support PSR-7.
- * Only requirement is to install guzzlehttp/psr7 package `composer require guzzlehttp/psr7`
- * @see https://developer.paddle.com/webhooks/signature-verification#verify-sdks?utm_source=dx&utm_medium=paddle-php-sdk
- */
-
 use GuzzleHttp\Psr7\ServerRequest;
 use Paddle\SDK\Entities\Event;
 use Paddle\SDK\Notifications\Events\TransactionUpdated;
 use Paddle\SDK\Notifications\Secret;
 use Paddle\SDK\Notifications\Verifier;
 
+// Capture the request
 $request = ServerRequest::fromGlobals();
 
-$isVerified = (new Verifier())->verify($request, new Secret('pdl_ntfset_01jq9k0z3j5m5tfaz5708xqk4z_sOC+7fruDmFOC3YekE33+hqYNvw8cXUA'));
+// Secret key from Paddle
+$secret = new Secret('pdl_ntfset_01jq9k0z3j5m5tfaz5708xqk4z_sOC+7fruDmFOC3YekE33+hqYNvw8cXUA');
+
+// Verify the request
+$isVerified = (new Verifier())->verify($request, $secret);
 
 if ($isVerified) {
-    echo "Webhook is verified\n";
+    echo json_encode(["message" => "Webhook is verified"]);
 
     $event = Event::fromRequest($request);
     $id = $event->notificationId;
@@ -32,6 +31,12 @@ if ($isVerified) {
     if ($event instanceof TransactionUpdated) {
         $transactionId = $event->transaction->id;
     }
+
+    // Ensure Paddle gets a valid response
+    http_response_code(200);
+    exit();
 } else {
-    echo "Webhook is not verified\n";
+    http_response_code(400);
+    echo json_encode(["error" => "Webhook is not verified"]);
+    exit();
 }
