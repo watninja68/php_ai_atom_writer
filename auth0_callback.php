097472d4b1a3c0aaf9266bab59da8131a7446189
karneeshkar <karneeshkar68@gmail.com>
2025-03-31 18:28:43 +0530

    small change 3

--- auth0_callback.php
+++ auth0_callback.php
@@ -2,7 +2,7 @@
 // This file acts solely as the entry point for the Auth0 redirect.
 // All logic is handled within auth0_handler.php.
 
-require_once _DIR_ . '/auth0_handler.php';
+require_once __DIR__ . '/auth0_handler.php';
 
 // The routing logic within auth0_handler.php checks for '?action=callback'
 // which is implicitly handled because this file is the redirect target.
