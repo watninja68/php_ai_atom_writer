0143f53819a5935c262ce3e8ec0f05cb98d90ff4
karneeshkar <karneeshkar68@gmail.com>
2025-03-31 17:33:41 +0530

    db changes

--- db_init.php
+++ db_init.php
@@ -1,9 +1,9 @@
 <?php
 $dbHost = "127.0.0.1"; // Or your DB host (e.g., localhost)
-$dbPort = "8111";      // Or 8111 if that's your MariaDB port
+$dbPort = "3306";      // Or 8111 if that's your MariaDB port
 $dbName = "write_db";   // <--- CHANGE THIS
 $dbUser = "root";      // <--- CHANGE THIS (Use a dedicated user if possible)
-$dbPass = "";          // <--- CHANGE THIS (Your DB user's password)
+$dbPass = "password";          // <--- CHANGE THIS (Your DB user's password)
 
 // Construct the DSN (Data Source Name)
 $dsn = "mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=utf8mb4";
