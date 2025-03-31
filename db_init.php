93fb61c747c9ed42a8d8e910a38206c25a79ac82
karneeshkar <karneeshkar68@gmail.com>
2025-03-31 17:52:11 +0530

    small change

--- db_init.php
+++ db_init.php
@@ -16,7 +16,12 @@ $options = [
 ];
 echo "hi";
  $try {
-     $pdo = new PDO($dsn, $dbUser, $dbPass, $options);
+
+    $pdo = new PDO($dsn, $dbUser, $dbPass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]);
 echo"Database done";
  } catch ( PDOException $e) {
 echo"Database failed";
