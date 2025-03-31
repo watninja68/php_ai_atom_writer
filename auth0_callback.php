19209e5b5af71e7cff0d4319b3596ace1dcb3103
karneeshkar <karneeshkar68@gmail.com>
2025-03-31 18:11:19 +0530

    db please work

--- auth0_callback.php
+++ auth0_callback.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/auth0_handler.php';
 // which is implicitly handled because this file is the redirect target.
 // If you didn't use '?action=callback' in the handler's routing,
 // you would explicitly call handleCallback() here.
+echo "wwfasdfas";
 $pdo = null;
 try {
     // Make sure $dsn, $dbUser, $dbPass are defined from db_init.php via the included handler
